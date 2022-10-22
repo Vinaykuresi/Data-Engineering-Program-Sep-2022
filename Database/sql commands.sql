34e9fb5dd12d9d2a4b440bd8387a1062f29db876
VinayKuresi <VinayKumar7565@gmail.com>
2022-10-22 06:58:03 +0530

    ddl : create, alter, drop, truncate, dml : select, insert, update and delete

--- Database/sql commands.sql
+++ Database/sql commands.sql
@@ -26,10 +26,72 @@ CREATE table Student(
 	StudentId  integer,
     Fname	varchar(10),
     Gender	char(1),
-    DOJ	date default curdate()
+    DOJ	date
 );
 
 select curdate();
+
+CREATE table Student(
+	StudentId  integer PRIMARY KEY,
+    Fname	varchar(10),
+    Gender	char(1),
+    DOJ	date
+);
+
+CREATE table Student(
+	StudentId  integer PRIMARY KEY,
+    Fname	varchar(10),
+    Gender	char(1) constraint Stud_gender_check check(Gender IN("M", "F")),
+    DOJ	date
+);
+
+CREATE TABLE Marks(
+	CourseId integer,
+    StudentId integer,
+	foreign key (StudentId) REFERENCES Student(StudentId) ,
+    MarksScored decimal(5,2)
+);
+
+drop table Marks;
+
+CREATE TABLE Marks(
+	CourseId integer,
+    StudentId integer,
+	foreign key (StudentId) REFERENCES Student(StudentId) ,
+    MarksScored decimal(5,2),
+    constraint marks_cid_pk primary key(CourseId, StudentId)
+);
+
+CREATE table Student(
+	StudentId  integer PRIMARY KEY,
+    Fname	varchar(10),
+    Gender	char(1) constraint Stud_gender_check check(Gender IN("M", "F")),
+    DOJ	date
+);
+
+INSERT INTO Student values(1001, "Vinay", "M", "1995-04-17");
+INSERT INTO Student values(1002, "Varma", "F", "1994-04-14");
+INSERT INTO Student values(1003, "Sukesh", "M", "1993-03-23");
+INSERT INTO Student values(1004, "Suresh", "M", "1994-07-13");
+INSERT INTO Student values(1005, "Gowtam", "M", "1992-11-05");
+
+select * from Student;
+
+alter table Student drop column DOJ;
+
+alter table Student Add Address varchar(20);
+
+alter table Student modify column Fname varchar(20);
+
+INSERT INTO Student(StudentId, Address) values(1006, "Tirupati, AP");
+
+update Student set Address = "Hyderabad, Telangana" where StudentId = 1003;
+
+delete from Student where StudentId = 1005;
+
+truncate table Student;
+
+
 
 
 
